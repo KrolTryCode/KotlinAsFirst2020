1c45e9f648152b9cd91061a258d520ed8286c2e9
Sana <amalsk0905@yandex.ru>
2021-09-08 02:20:18 +0300

    Первая решенная задача, тест котоеда

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -65,7 +65,11 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
+    val seconds=seconds+minutes*60+hours*60*60
+    return(seconds)
+}
+
 
 /**
  * Тривиальная (1 балл)
