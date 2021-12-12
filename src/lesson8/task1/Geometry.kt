72a469a45e1dce818fc060e21b133f94ba30df84
Sana <amalsk0905@yandex.ru>
2021-12-12 21:56:55 +0300

    8 lesson, geometry

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -186,7 +186,7 @@ class Line private constructor(val b: Double, val angle: Double) {
 fun lineBySegment(s: Segment): Line {
     val x1 = s.end.x - s.begin.x
     val y1 = s.end.y - s.begin.y
-    var angle = acos(abs(x1) / sqrt(x1 * x1 + y1 * y1))
+    var angle = atan(abs(y1 / x1))
     if (angle < 0.0) angle += PI
     return Line(s.begin, angle)
 }
